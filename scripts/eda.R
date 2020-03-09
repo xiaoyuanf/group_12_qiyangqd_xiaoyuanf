f299249f0c0a8fd18841f90ec515d5223b699363
Qi Yang <qiyangqd@gmail.com>
2020-03-08 23:00:46 -0700

    Plot 4 and 5

--- scripts/eda.R
+++ scripts/eda.R
@@ -4,7 +4,7 @@
 "This script carries out the exploration data analysis (EDA) for the project Beijing PM2.5. 
 It produces five plots: 1) 2) 3) 4) Season VS. [PM2.5] and 5)Year VS. [PM2.5].
 
-Usage: load_data.R --file_path<file_path> --image_path<image_path>
+Usage: load_data.R --image_path=<image_path>
 "-> doc
 
 # Load packages
@@ -16,40 +16,79 @@ suppressMessages(library(ggplot2))
 opt <- docopt(doc)
 
 # Main function
-main <- function(){
+main <- function(image_path){
   
   # read in the dataset
-  # these two lines will be replaced by one line: df_clean <- read.csv(here("scripts", "clean_data"))
-  df <- read.csv(file_path)
-  df_clean<- na.omit(df)
+  df_clean <- na.omit(read.csv(here("data", "raw_data.csv"))) # should be replaced by "clean_data"
   
-  # histogram: season VS. PM2.5
+  # 1.Correllogram: `DEWP`, `TEMP`, `PRES`, and `PM2.5`.
+  
+  
+  
+  # 2.Faceted histogram: distribution of [PM2.5] in different wind directions.
+  
+  
+  
+  # 3.Heat map: hourly [PM2.5] from 2013 to 2014. 
+  
+  
+  
+  # 4.Bar chart: season VS. PM2.5
   ## add "season" to df_clean
-  df_hist <- df_clean %>% 
+  df_bar <- df_clean %>% 
     mutate(season = case_when(month == 12 ~ 'Winter', month >= 9 ~ 'Autumn', month >= 6 ~ 'Summer', month >= 3 ~ 'Spring',TRUE ~ 'Winter'))  # Group dates to seasons
   ## calculate the mean [PM2.5] for each season
-  df_hist = aggregate(df_hist$pm2.5,
-                      by = list(df_hist$season),
+  df_bar = aggregate(df_bar$pm2.5,
+                      by = list(df_bar$season),
                       FUN = mean) 
-  ## rename the new df_hist
-  df_hist = rename(df_hist, c("season" = "Group.1", "PM2.5" = "x")) 
+  ## rename season and PM2.5 column
+  df_bar = rename(df_bar, c("season" = "Group.1", "PM2.5" = "x")) 
+  ## reorder the season
+  df_bar$season = factor(df_bar$season, levels = c("Spring", "Summer", "Autumn", "Winter"))
   ## plotting
-  ggplot(data = df_hist, aes(x=season, y = PM2.5)) +
+  season_PM2.5 <- ggplot(data = df_bar, aes(x=season, y = PM2.5)) +
       geom_bar(stat="identity")+
-      coord_cartesian(ylim=c(80,120))+
+      coord_cartesian(ylim=c(80,120))+ # limite the range of [PM2.5]
       labs(x = "Season", 
            y = "[PM2.5]",
-           title = "PM2.5 VS Season") +
-      theme_classic()+
-    ggsave(here("images","season_vs_PM2.5.png"))
-  
+           title = "PM2.5 VS Season")+
+      theme_classic()
+    ggsave(plot = season_PM2.5, filename = "season_PM2.5.png", path = image_path)
+
+  # 5.Line chart: year VS. PM2.5
+  ## add "date" column
+    df_year_change <- df_clean
+    df_year_change$date = as.Date(with(df_year_change, paste(year, month, day,sep="-")), "%Y-%m-%d") # Combine day, month and year to date
+  ## calculate the mean for each date
+    df_year_change = aggregate(df_year_change$pm2.5,
+                               by = list(df_year_change$date),
+                               FUN = mean) # Mean [PM2.5] of each date
+  ## rename date and PM2.5 column
+    df_year_change = rename(df_year_change, c("date" = "Group.1", "PM2.5"="x"))
+  ## plotting
+    year_PM2.5 = ggplot(data = df_year_change) +
+        geom_line(aes(x=date, y=PM2.5), 
+                  alpha = 0.6,
+                  size = 0.6) +
+      geom_vline(xintercept = as.numeric(as.Date("2013-09-01")), linetype=4, color = "blue", size = 1) +
+    # add a vertical line showing the time when Chinese overnment launched the plan to control PM2.5
+        labs(x = "Time", 
+             y = "[PM2.5]",
+             title = "[PM2.5] VS Time") +
+        theme_classic()
+    ggsave(plot = year_PM2.5, filename = "year_PM2.5.png", width=12, height=4, path = image_path)
+    
+    
+    
   # message for users
-  print("The script has been run successfully!")
+  print("EDA has run successfully!")
 }
 
 
-#' @param file_path is the full path name to the clean data file
+#' @param image_path is the full path name to the folder where users would like to save the images. 
+#' "images" would be a good choice.
+#' 
 
 
 # Tests
-main(opt$file_path, opt$)
+main(opt$image_path)
